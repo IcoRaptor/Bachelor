7318d736ab46ee60c3b991ff6dfa0a133e758cec
Tim Redmann <timloris95@gmail.com>
2018-04-09 13:04:57 +0200

    impl2

---
+++ Assets/Scripts/AI/GOAP/Plan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AI.GOAP
+{
+    public class Plan
+    {
+        #region Variables
+
+#pragma warning disable 0414
+        private LinkedList<BaseAction> _plan =
+            new LinkedList<BaseAction>();
+#pragma warning restore
+
+        #endregion
+    }
+}
